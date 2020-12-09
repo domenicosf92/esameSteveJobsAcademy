e68baa47022fe9b69bca5725394676177663c16b
Domenico Garozzo <domenico.garozzo@beta80group.it>
2020-12-09 17:58:19 +0100

    Out of project- updated home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  host: {
-    class: 'app'
-  }
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'esameSteveJobsAcademy';
